1cfb72801df7f2b0e10bd4b2b63f6245ef1adf6c
akp4321 <ankit.pathak52@gmail.com>
2025-08-30 00:29:55 +0530

    updated BasePage.java

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,9 +2,6 @@ package pages;
 
 import core.ThreadLocalDriverManager;
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.PerformsTouchActions;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
@@ -99,78 +96,6 @@ public class BasePage {
         element.click();
     }
 
-
-    public void swipeUp(int durationInMillis) {
-        Dimension size = driver.manage().window().getSize();
-        int startX = size.width / 2;
-        int startY = (int) (size.height * 0.8);
-        int endY = (int) (size.height * 0.2);
-
-        performSwipe(startX, startY, startX, endY, durationInMillis);
-    }
-
-    private void performSwipe(int startX, int startY, int endX, int endY, int durationInMillis) {
-        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
-        Sequence swipe = new Sequence(finger, 1);
-
-        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
-        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(durationInMillis), PointerInput.Origin.viewport(), endX, endY));
-        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-
-        driver.perform(Arrays.asList(swipe));
-    }
-
-    public void swipeDown(int durationInMillis) {
-        Dimension size = driver.manage().window().getSize();
-        int startX = size.width / 2;
-        int startY = (int) (size.height * 0.2);
-        int endY = (int) (size.height * 0.8);
-
-        performSwipe(startX, startY, startX, endY, durationInMillis);
-    }
-
-    public void swipeLeft(int durationInMillis) {
-        Dimension size = driver.manage().window().getSize();
-        int startY = size.height / 2;
-        int startX = (int) (size.width * 0.8);
-        int endX = (int) (size.width * 0.2);
-
-        performSwipe(startX, startY, endX, startY, durationInMillis);
-    }
-
-    public void swipeRight(int durationInMillis) {
-        Dimension size = driver.manage().window().getSize();
-        int startY = size.height / 2;
-        int startX = (int) (size.width * 0.2);
-        int endX = (int) (size.width * 0.8);
-
-        performSwipe(startX, startY, endX, startY, durationInMillis);
-    }
-
-    public boolean scrollDown() {
-        if (platform.equalsIgnoreCase("iOS")) {
-            Map<String, Object> scroll = new HashMap<>();
-            scroll.put("direction", "down");
-            driver.executeScript("mobile: scroll", scroll);
-        } else {
-
-            Dimension size = driver.manage().window().getSize();
-            int startY = (int) (size.height * 0.8);
-            int endY = (int) (size.height * 0.2);
-            int startX = size.width / 2;
-
-            PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
-            Sequence swipe = new Sequence(finger, 1);
-            swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
-            swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-            swipe.addAction(finger.createPointerMove(Duration.ofMillis(800), PointerInput.Origin.viewport(), startX, endY));
-            swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-            driver.perform(Collections.singletonList(swipe));
-        }
-        return true;
-    }
-
     public boolean scrollUntilElementVisible(By locator, int maxScrolls, String direction) {
         int scrollCount = 0;
         while (scrollCount < maxScrolls) {
@@ -186,27 +111,6 @@ public class BasePage {
             scrollCount++;
         }
         throw new RuntimeException("❌ Element not found after " + maxScrolls + " scrolls");
-    }
-
-    public void scrollUp() {
-        Dimension size = driver.manage().window().getSize();
-
-        int screenWidth = size.getWidth();
-        int screenHeight = size.getHeight();
-
-        int startX = screenWidth / 2;
-        int startY = (int) (screenHeight * 0.2); // near bottom
-        int endY = (int) (screenHeight * 0.8);   // near top
-
-        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
-
-        Sequence swipe = new Sequence(finger, 1);
-        swipe.addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY));
-        swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(600), PointerInput.Origin.viewport(), startX, endY));
-        swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-
-        this.driver.perform(Arrays.asList(swipe));
     }
 
     public void scrolling (String direction) {
